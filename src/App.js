65e716ed193b3b2ad239f63f421ee08c7cea9742
Raniyaaaa <raniya182002@gmail.com>
2024-10-25 13:29:40 +0530

    using useState to manage the visiblility of Cart and implemented the close button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import HeaderCartButton from "./components/Layout/HeaderCartButton";
 function App() {
+
+  const [CartIsVisilble, setCartIsVisible]=useState(false)
+
+  const showCartHandler=()=>{
+    setCartIsVisible(true);
+  }
+
+  const hideCartHandler=()=>{
+    setCartIsVisible(false)
+  }
   return (
     <React.Fragment>
-      <Cart/>
-      <Header/>
+      {CartIsVisilble && <Cart  onCloseCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
